6e0ac6297a06feca3f2995baa8acf9ed6674bde8
Misha Vialov <mishavjalov48@gmail.com>
2021-02-07 23:31:55 +0300

    initial commit

---
+++ tracker/trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackerApp.swift
+//  tracker
+//
+//  Created by misha on 07.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
